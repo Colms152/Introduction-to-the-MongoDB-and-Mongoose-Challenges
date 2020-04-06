e59c407439614b42feb4ec879974a4cc3c063610
Colm Sheehan <colms152@gmail.com>
2020-04-06 22:30:38 +0100

    12 of 12

--- myApp.js
+++ myApp.js
@@ -324,7 +324,15 @@ var removeManyPeople = function(done) {
 var queryChain = function(done) {
   var foodToSearch = "burrito";
   
-  done(null/*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: "asc"}).limit(2).select("-age").exec(function(err, data) {
+    
+            
+    if (err) {
+      return done(err)
+    }
+
+    done(null, data);
+  });
 };
 
 /** **Well Done !!**
